6c0f8ca0dc20046120a5be4cf59d028118baaa6c
592k <kweonok94@gmail.com>
2022-06-06 20:37:02 +0900

    fix: fixed error

--- src/model.py
+++ src/model.py
@@ -1,2 +1,16 @@
-from preprocessing import prep
+import numpy as np
+import pandas as pd
 
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
+
+clf_d = DecisionTreeRegressor()
+clf_d.fit(X_train, y_train)
+mean_absolute_error(y_val, clf_d.predict(X_val)), mean_squared_error(y_val, clf_d.predict(X_val))
+
+predict = clf_d.predict(test)
+submission['count'] = predict
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,44 +1,38 @@
 import numpy as np
 import pandas as pd
 
-def preprocessing():
 
-    base_path = '../data/'
-    train = pd.read_csv(base_path+'/train.csv', encoding='cp949')
-    test = pd.read_csv(base_path+'/test.csv', encoding='cp949')
-    submission = pd.read_csv(base_path+'/submission.csv', encoding='cp949')
+base_path = '../data/'
+train = pd.read_csv(base_path+'/train.csv', encoding='cp949')
+test = pd.read_csv(base_path+'/test.csv', encoding='cp949')
+submission = pd.read_csv(base_path+'/submission.csv', encoding='cp949')
 
-    # drop train.na
-    train = train.drop(train[train.hour_bef_temperature.isnull()==True].index)
+# drop train.na
+train = train.drop(train[train.hour_bef_temperature.isnull()==True].index)
 
-    # concat train & test
-    df = pd.concat([train.drop(columns=['count']), test])
+# concat train & test
+df = pd.concat([train.drop(columns=['count']), test])
 
-    # train.fillna
-    def fill_nan(dafa, col):
-        dafa[col].fillna(df[col].median(), inplace=True)
+# train.fillna
+def fill_nan(dafa, col):
+    dafa[col].fillna(df[col].median(), inplace=True)
 
-    # test.fillna
-    def test_fill(a):
-        test.loc[653,a] = df[df.hour==19][a].median() 
-
-
-    col_list = ['hour_bef_pm2.5','hour_bef_pm10','hour_bef_ozone', 'hour_bef_windspeed']
+# test.fillna
+def test_fill(a):
+    test.loc[653,a] = df[df.hour==19][a].median() 
 
 
-    for i in col_list:
-        fill_nan(train, i)
+col_list = ['hour_bef_pm2.5','hour_bef_pm10','hour_bef_ozone', 'hour_bef_windspeed']
 
-    for t in col_list[:-1]:
-        fill_nan(test, t)
 
-    test_fill(['hour_bef_humidity','hour_bef_temperature','hour_bef_precipitation','hour_bef_windspeed','hour_bef_visibility'])
+for i in col_list:
+    fill_nan(train, i)
 
-    # id drop
-    train.drop(columns=['id'], inplace=True)
-    test.drop(columns=['id'], inplace=True)
+for t in col_list[:-1]:
+    fill_nan(test, t)
 
-if __name__ == '__preprocessing.py__':
-    preprocessing()
+test_fill(['hour_bef_humidity','hour_bef_temperature','hour_bef_precipitation','hour_bef_windspeed','hour_bef_visibility'])
 
-prep = preprocessing()
+# id drop
+train.drop(columns=['id'], inplace=True)
+test.drop(columns=['id'], inplace=True)
